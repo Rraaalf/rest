70aef72c382325e0340370e5fe757142af23988a
Rraaalf <kymyass@gmail.com>
2024-02-25 01:03:32 +0300

    changed: deleted currency check

--- src/test/java/ru/netology/rest/MobileBankApiTestV4.java
+++ src/test/java/ru/netology/rest/MobileBankApiTestV4.java
@@ -20,8 +20,6 @@ class MobileBankApiTestV4 {
                 .then()
                 .statusCode(200)
                 .body(matchesJsonSchemaInClasspath("accounts.schema.json"))
-                .body("[0].currency", equalTo("RUB"))
-                .body("[1].currency", equalTo("USD"))
         ;
     }
 }
